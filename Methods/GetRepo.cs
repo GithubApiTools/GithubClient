caa6fc11dd88e3b5e7ec10997fbb9b6114ea856d
Jeffrey Patton <jeffrey@patton-tech.com>
2022-08-12 09:04:17 -0500

    Issue-4: Removed the GithubUrl param as it's no longer required.

--- Methods/GetRepo.cs
+++ Methods/GetRepo.cs
@@ -14,10 +14,9 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Owner">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <returns>A collection of repository objects</returns>
-        public static async Task<List<Repository>>? GetRepositories(string PAT, string GithubUrl, string Owner)
+        public static async Task<List<Repository>>? GetRepositories(string PAT, string Owner)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = Repository.GetApiUrl();
@@ -32,19 +31,18 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Owner">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="PerPage">The number of results per page (max 100).</param>
         /// <param name="Page">Page number of the results to fetch</param>
         /// <returns>A collection of repository objects</returns>
-        public static async Task<List<Repository>> GetRepositories(string PAT, string GithubUrl, string Owner, int PerPage = 30, int Page = 1)
+        public static async Task<List<Repository>> GetRepositories(string PAT, string Owner, int PerPage = 30, int Page = 1)
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(GithubUrl);
+            client.BaseAddress = Repository.GetApiUrl();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Repository.GetHeader()));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
             client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
-            string RequestUrl = GithubUrl + "/repos/" + Owner + "/repos?per_page=" + PerPage + "&page=" + Page;
+            string RequestUrl = Repository.GetApiUrl() + "/repos/" + Owner + "/repos?per_page=" + PerPage + "&page=" + Page;
             Task<Stream> Response = client.GetStreamAsync(new Uri(RequestUrl));
             return JsonSerializer.Deserialize<List<Repository>>(await Response);
         }
@@ -53,21 +51,20 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Owner">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="PerPage">The number of results per page (max 100).</param>
         /// <param name="Page">Page number of the results to fetch</param>
         /// <param name="Type">Specifies the types of repositories you want returned.</param>
         /// <param name="Direction">The order to sort by. Default: asc</param>
         /// <returns>A collection of repository objects</returns>
-        public static async Task<List<Repository>> GetRepositories(string PAT, string GithubUrl, string Owner, int PerPage = 30, int Page = 1, string Type = "all", string Direction = "asc")
+        public static async Task<List<Repository>> GetRepositories(string PAT, string Owner, int PerPage = 30, int Page = 1, string Type = "all", string Direction = "asc")
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(GithubUrl);
+            client.BaseAddress = Repository.GetApiUrl();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Repository.GetHeader()));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
             client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
-            string RequestUrl = GithubUrl + "/repos/" + Owner + "/repos?per_page=" + PerPage + "&page=" + Page + "&type=" + Type + "&direction=" + Direction;
+            string RequestUrl = Repository.GetApiUrl() + "/repos/" + Owner + "/repos?per_page=" + PerPage + "&page=" + Page + "&type=" + Type + "&direction=" + Direction;
             Task<Stream> Response = client.GetStreamAsync(new Uri(RequestUrl));
             return JsonSerializer.Deserialize<List<Repository>>(await Response);
         }
@@ -76,11 +73,10 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Owner">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="Name">The name of the repository. The name is not case sensitive.</param>
         /// <returns>A repository object</returns>
-        public static async Task<Repository> GetRepository(string PAT, string GithubUrl, string Owner, string Name)
+        public static async Task<Repository> GetRepository(string PAT, string Owner, string Name)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = Repository.GetApiUrl();
@@ -95,10 +91,9 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Org">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <returns>A collection of repository objects</returns>
-        public static async Task<List<Repository>> GetOrgRepositories(string PAT, string GithubUrl, string Org)
+        public static async Task<List<Repository>> GetOrgRepositories(string PAT, string Org)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = Repository.GetApiUrl();
@@ -113,12 +108,11 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Org">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="PerPage">The number of results per page (max 100).</param>
         /// <param name="Page">Page number of the results to fetch</param>
         /// <returns>A collection of repository objects</returns>
-        public static async Task<List<Repository>> GetOrgRepositories(string PAT, string GithubUrl, string Org, int PerPage = 30, int Page = 1)
+        public static async Task<List<Repository>> GetOrgRepositories(string PAT, string Org, int PerPage = 30, int Page = 1)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = Repository.GetApiUrl();
@@ -133,17 +127,16 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Org">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="PerPage">The number of results per page (max 100).</param>
         /// <param name="Page">Page number of the results to fetch</param>
         /// <param name="Type">Specifies the types of repositories you want returned.</param>
         /// <param name="Direction">The order to sort by. Default: asc</param>
         /// <returns>A collection of repository objects</returns>
-        public static async Task<List<Repository>> GetOrgRepositories(string PAT, string GithubUrl, string Org, int PerPage = 30, int Page = 1, string Type = "all", string Direction = "asc")
+        public static async Task<List<Repository>> GetOrgRepositories(string PAT, string Org, int PerPage = 30, int Page = 1, string Type = "all", string Direction = "asc")
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(GithubUrl);
+            client.BaseAddress = Repository.GetApiUrl();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Repository.GetHeader()));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
             client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
@@ -155,14 +148,13 @@ namespace GithubClient.Methods
         /// </summary>
         /// <seealso href="https://docs.github.com/en/rest/repos/repos">Github Docs : Repos</seealso>
         /// <param name="PAT">Personal Access Token</param>
-        /// <param name="GithubUrl">Github API Url</param>
         /// <param name="Org">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="Name">The name of the repository. The name is not case sensitive.</param>
         /// <returns>A repository object</returns>
-        public static async Task<Repository> GetOrgRepository(string PAT, string GithubUrl, string Org, string Name)
+        public static async Task<Repository> GetOrgRepository(string PAT, string Org, string Name)
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(GithubUrl);
+            client.BaseAddress = Repository.GetApiUrl();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Repository.GetHeader()));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
             client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
