84f99d5640969c5750e2b55ea22b2a2281c59e04
Jeffrey Patton <jeffrey@patton-tech.com>
2022-08-16 14:35:32 -0500

    issue-1: Added and implemented ContentClient

--- Methods/GetContent.cs
+++ Methods/GetContent.cs
@@ -1,9 +1,37 @@
-﻿using System.Text.Json;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
+using GithubClient.Git;
 using GithubClient.Repositories;
+using System.Net.Http.Json;
+using System.Xml.Linq;
 
 namespace GithubClient.Methods
 {
+    internal class ContentClient
+    {
+        private readonly HttpClient _httpClient;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpClient"></param>
+        /// <param name="baseAddress"></param>
+        /// <param name="header"></param>
+        /// <param name="pat"></param>
+        public ContentClient(HttpClient httpClient, Uri baseAddress, string header, string pat)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = baseAddress;
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(header));
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", pat);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<ContentDirectory>> GetStreamAsync(Uri endpoint) =>
+            await _httpClient.GetFromJsonAsync<IEnumerable<ContentDirectory>>(endpoint);
+    }
     /// <summary>
     /// A Collection of methods for working with Content objects in the Github API
     /// </summary>
@@ -17,17 +45,11 @@ namespace GithubClient.Methods
         /// <param name="Owner">The account owner of the repository. This can also be the organization name. The name is not case sensitive.</param>
         /// <param name="Name">The name of the repository. The name is not case sensitive.</param>
         /// <returns>A DirectoryContent object</returns>
-        public static async Task<List<ContentDirectory>> GetContents(string PAT, string Owner, string Name)
+        public static async Task<IEnumerable<ContentDirectory>>? GetContents(string PAT, string Owner, string Name)
         {
-            HttpClient client = new()
-            {
-                BaseAddress = ContentDirectory.GetApiUrl()
-            };
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ContentDirectory.GetHeader()));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
-            client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
-            Task<Stream> Response = client.GetStreamAsync(ContentDirectory.GetEndpointUrl(Owner, Name));
-            return JsonSerializer.Deserialize<List<ContentDirectory>>(await Response);
+            ContentClient client = new(new HttpClient(), Blob.GetApiUrl(), Blob.GetHeader(), PAT);
+            IEnumerable<ContentDirectory> Response = await client.GetStreamAsync(ContentDirectory.GetEndpointUrl(Owner, Name));
+            return Response;
         }
         /// <summary>
         /// Returns a content object from the Github API
@@ -37,20 +59,14 @@ namespace GithubClient.Methods
         /// <param name="repository">A Repository object</param>
         /// <param name="Ref">The name of the commit/branch/tag. Default: the repository’s default branch (usually master)</param>
         /// <returns>A DirectoryContent object</returns>
-        public static async Task<List<ContentDirectory>>? GetContents(string PAT, Repository repository, string Ref = "main")
+        public static async Task<IEnumerable<ContentDirectory>>? GetContents(string PAT, Repository repository, string Ref = "main")
         {
-            HttpClient client = new()
-            {
-                BaseAddress = ContentDirectory.GetApiUrl()
-            };
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ContentDirectory.GetHeader()));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
-            client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
+            ContentClient client = new(new HttpClient(), Blob.GetApiUrl(), Blob.GetHeader(), PAT);
             if (repository.ContentsUrl != null)
             {
                 string RequestUrl = repository.ContentsUrl.Replace("/{+path}", "") + "?ref=" + Ref;
-                Task<Stream> Response = client.GetStreamAsync(new Uri(RequestUrl));
-                return JsonSerializer.Deserialize<List<ContentDirectory>>(await Response);
+                IEnumerable<ContentDirectory> Response = await client.GetStreamAsync(new Uri(RequestUrl));
+                return Response;
             }
             return null;
         }
@@ -61,19 +77,13 @@ namespace GithubClient.Methods
         /// <param name="PAT">Personal Access Token</param>
         /// <param name="content">A DirectoryContent object</param>
         /// <returns>A DirectoryContent object</returns>
-        public static async Task<List<ContentDirectory>>? GetContents(string PAT, ContentDirectory content)
+        public static async Task<IEnumerable<ContentDirectory>>? GetContents(string PAT, ContentDirectory content)
         {
-            HttpClient client = new()
-            {
-                BaseAddress = ContentDirectory.GetApiUrl()
-            };
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ContentDirectory.GetHeader()));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
-            client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
+            ContentClient client = new(new HttpClient(), Blob.GetApiUrl(), Blob.GetHeader(), PAT);
             if (content.Url != null)
             {
-                Task<Stream> Response = client.GetStreamAsync(content.Url);
-                return JsonSerializer.Deserialize<List<ContentDirectory>>(await Response);
+                IEnumerable<ContentDirectory> Response = await client.GetStreamAsync(content.Url);
+                return Response;
             }
             return null;
         }
