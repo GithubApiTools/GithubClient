1de9096d28a20cbfafe55bcbf4ff4543f1eea847
Jeffrey Patton <jeffrey@patton-tech.com>
2022-08-16 14:21:08 -0500

    issue-1: Added a blobClient and implemented in methods.

--- Methods/GetBlob.cs
+++ Methods/GetBlob.cs
@@ -2,9 +2,37 @@
 using System.Net.Http.Headers;
 using GithubClient.Git;
 using GithubClient.Repositories;
+using System.Net.Http.Json;
+using System.Xml.Linq;
 
 namespace GithubClient.Methods
 {
+    internal class BlobClient
+    {
+        private readonly HttpClient _httpClient;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpClient"></param>
+        /// <param name="baseAddress"></param>
+        /// <param name="header"></param>
+        /// <param name="pat"></param>
+        public BlobClient(HttpClient httpClient, Uri baseAddress, string header, string pat)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = baseAddress;
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(header));
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", pat);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Blob>> GetStreamAsync(Uri endpoint) =>
+            await _httpClient.GetFromJsonAsync<IEnumerable<Blob>>(endpoint);
+    }
     /// <summary>
     /// A Collection of methods for working with Blob objects in the Github API
     /// </summary>
@@ -19,17 +47,11 @@ namespace GithubClient.Methods
         /// <param name="Name">The name of the repository. The name is not case sensitive.</param>
         /// <param name="Sha">Secure hashing algorithm</param>
         /// <returns>A blob object</returns>
-        public static async Task<Blob> GetBlob(string PAT, string Owner, string Name, string Sha)
+        public static async Task<IEnumerable<Blob>>? GetBlob(string PAT, string Owner, string Name, string Sha)
         {
-            HttpClient client = new()
-            {
-                BaseAddress = Blob.GetApiUrl()
-            };
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Blob.GetHeader()));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
-            client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
-            Task<Stream> Response = client.GetStreamAsync(Blob.GetEndpoint(Owner, Name, Sha));
-            return JsonSerializer.Deserialize<Blob>(await Response);
+            BlobClient client = new(new HttpClient(), Blob.GetApiUrl(), Blob.GetHeader(), PAT);
+            IEnumerable<Blob> Response = await client.GetStreamAsync(Blob.GetEndpoint(Owner, Name, Sha));
+            return Response;
         }
         /// <summary>
         /// Returns a blob object from the Github API
@@ -38,20 +60,13 @@ namespace GithubClient.Methods
         /// <param name="PAT">Personal Access Token</param>
         /// <param name="content">The contents of a file or directory in a repository.</param>
         /// <returns>A blob object</returns>
-        public static async Task<Blob>? GetBlob(string PAT, ContentDirectory content)
+        public static async Task<IEnumerable<Blob>>? GetBlob(string PAT, ContentDirectory content)
         {
-            HttpClient client = new()
-            {
-                BaseAddress = Blob.GetApiUrl()
-            };
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Blob.GetHeader()));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", PAT);
-            client.DefaultRequestHeaders.Add("User-Agent", "Github Api Client");
+            BlobClient client = new(new HttpClient(), Blob.GetApiUrl(), Blob.GetHeader(), PAT);
             if (content.GitUrl != null)
             {
-                string RequestUrl = content.GitUrl;
-                Task<Stream> Response = client.GetStreamAsync(new Uri(RequestUrl));
-                return JsonSerializer.Deserialize<Blob>(await Response);
+                IEnumerable<Blob> Response = await client.GetStreamAsync(new Uri(content.GitUrl));
+                return Response;
             }
             return null;
         }
